8573a9f6fd25315397ffa23fd6787cd9dd992441
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-12-16 17:08:05 +0000

    Initial Commit

---
+++ src/main/java/ie/atu/robotpublish/RobotPublishApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.robotpublish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotPublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotPublishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/robotpublish/RobotPublishApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.robotpublish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotPublishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
