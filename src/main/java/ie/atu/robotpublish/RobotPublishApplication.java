69c3861e90f05b9965ac22f4d0f54a659ffacd73
G00392765@atu.ie <G00392765@atu.ie>
2024-01-22 10:23:46 +0000

    Enabled Feign Clients

--- src/main/java/ie/atu/robotpublish/RobotPublishApplication.java
+++ src/main/java/ie/atu/robotpublish/RobotPublishApplication.java
@@ -2,8 +2,11 @@ package ie.atu.robotpublish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RobotPublishApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +14,4 @@ public class RobotPublishApplication {
 	}
 
 }
+
