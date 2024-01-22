624dac2af655e9435851d9aa05614c4bf8b198d0
G00392765@atu.ie <G00392765@atu.ie>
2024-01-22 10:25:59 +0000

    Created sliderData class

---
+++ src/main/java/ie/atu/robotpublish/Models/SliderData.java
@@ -0,0 +1,16 @@
+package ie.atu.robotpublish.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SliderData {
+    int servo1;
+    int servo2;
+    int servo3;
+    int servo4;
+    int servo5;
+}
